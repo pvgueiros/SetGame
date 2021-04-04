66bf72020f33103986c6d398fa51f82d747de9cc
Paulinha <pvgueiros@gmail.com>
2021-04-04 14:18:46 -0300

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Paula Vasconcelos Gueiros on 04/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
